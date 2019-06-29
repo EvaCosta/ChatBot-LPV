e7cc71b9bde650b05c996fe9d0a978140249e07b
Daniel <Daniel@192.168.56.1>
2019-06-29 17:27:46 -0300

    Adição de um exemplo de código.

--- ChatBot-LPV/ChatBot-LPV/src/application/SampleController.java
+++ ChatBot-LPV/ChatBot-LPV/src/application/SampleController.java
@@ -1,5 +1,19 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
+import lpv.chatbot.classes.Label;
+
 public class SampleController {
 	
-}
+	@FXML  private TextArea exibirTextoId;
+	
+	@FXML
+	public void initialize() {
+		Label label = new Label();
+		exibirTextoId.setText(label.toString());
+		exibirTextoId.setFont(new Font("Arial Italic", 15));
+	}
+	
+}//class SampleController 
