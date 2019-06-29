27ca497bdb8702cec4e51c3c88a693537b8c0e47
Daniel <Daniel@192.168.56.1>
2019-06-29 10:31:45 -0300

    Atualização da Interface

--- ChatBot-LPV/ChatBot-LPV/src/application/Main.java
+++ ChatBot-LPV/ChatBot-LPV/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
