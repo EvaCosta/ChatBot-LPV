5b8abccb2b2bb296ec8190de5b74c73a27926364
RamonGiovane <ramon.giovane@gmail.com>
2019-07-03 10:41:48 -0300

    [UP] Melhorando interface

--- ChatBot - LPV/src/lpv/tp/chatbot/controller/ChatBotController.java
+++ ChatBot - LPV/src/lpv/tp/chatbot/controller/ChatBotController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -18,6 +19,8 @@ public class ChatBotController {
 	@FXML private VBox chatPane;
 	@FXML private TextField msgField;
 	@FXML private Button btnEnviar;
+	@FXML private ScrollPane scrollPane;
+	
 	private static double chatPaneWidth = 0;
 	private static Scene mainScene;
 
@@ -25,12 +28,21 @@ public class ChatBotController {
 	@FXML
 	public void initialize() {
 		chatPaneWidth = chatPane.getPrefWidth();
+		
 		msgField.addEventHandler(KeyEvent.KEY_PRESSED, (e)->{
 			if(e.getCode() == KeyCode.ENTER){
 				mainScene = msgField.getScene();
 				enviarMensagem();
+				
 			}
 		});
+		
+		chatPane.heightProperty().addListener((observable, oldVal, newVal)->{
+			
+			scrollPane.setVvalue((double)newVal);
+		
+		});
+		msgField.requestFocus();
 	}
 
 	@FXML
@@ -38,24 +50,39 @@ public class ChatBotController {
 		Button btn = (Button)e.getSource();
 		mainScene = btn.getScene();
 		enviarMensagem();
+		
 
 	}
 
 	private void enviarMensagem(){
 		if(msgField.getText().length() > 0 && !msgField.getText().matches("\\s*")){
 			chatPane.getChildren().add(new Mensagem(TipoMensagem.PERGUNTA, new Label(msgField.getText().trim())));
-			integracaoWatson = new IntegracaoWatson(msgField.getText());
+			
+			Mensagem msg = new Mensagem(TipoMensagem.RESPOSTA);
+			msg.adicionarConteudoExpandivel("sadsad", new Label("fdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefew"), new Button("dsdwedwe"));
+			msg.adicionarConteudoExpandivel("sadsad", new Label("fdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefewfdffwefew"), new Button("dsdwedwe"));
+			
+			chatPane.getChildren().add(msg);
+			//integracaoWatson = new IntegracaoWatson(msgField.getText());
 		}
 		msgField.setText("");
+		msgField.requestFocus();
+	
 	}
 
 
 	public static double getChatPaneWidth() {
 		return chatPaneWidth;
 	}
+	
+	public static void setChatPaneWidth(double width) {
+		chatPaneWidth = width;
+	}
+	
 
 	public static Scene getMainScene() {
 		return mainScene;
 	}
+	
 
 }//class ChatBotController
--- ChatBot - LPV/src/lpv/tp/chatbot/ui/Mensagem.java
+++ ChatBot - LPV/src/lpv/tp/chatbot/ui/Mensagem.java
@@ -3,8 +3,9 @@ package lpv.tp.chatbot.ui;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Accordion;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import lpv.tp.chatbot.controller.ChatBotController;
@@ -15,32 +16,49 @@ public class Mensagem extends VBox {
 	}
 
 	private Color cor;
-	private HBox pane = new HBox();
+	private VBox pane = new VBox();
 	private TipoMensagem tipoMensagem;
+	private Accordion accordion;
 
 	public Mensagem(TipoMensagem tipoMensagem){
+		this.tipoMensagem = tipoMensagem;
+		estilizarCSS(tipoMensagem);
+	}
+
+	private void estilizarCSS(TipoMensagem tipoMensagem) {
 		getChildren().add(pane);
 		setFillWidth(false);
-		this.tipoMensagem = tipoMensagem;
+		
 		if(tipoMensagem == TipoMensagem.PERGUNTA){
 			cor = Color.rgb(179, 229, 252);
 			setAlignment(Pos.CENTER_RIGHT);
 			setMargin(pane, new Insets(10, 10, 0, 0));
-			pane.setStyle("-fx-background-color: rgb(179, 229, 252)");
+			pane.setStyle("-fx-background-color: rgb(0, 188, 212)");
 		}else if(tipoMensagem == TipoMensagem.RESPOSTA){
 			cor = Color.rgb(240, 244, 195);
 			setAlignment(Pos.CENTER_LEFT);
 			setMargin(pane, new Insets(10, 0, 0, 10));
-			pane.setStyle("-fx-background-color: rgb(240, 244, 195)");
+			pane.setStyle("-fx-background-color: rgb(123,104,238)");
 
 		}
 		pane.applyCss();
 
 	}
 
-	public Mensagem(TipoMensagem tipoMensagem, Node node){
+	public Mensagem(TipoMensagem tipoMensagem, Node conteudoMensagem){
 		this(tipoMensagem);
-		adicionarConteudo(node);
+		adicionarConteudo(conteudoMensagem);
+	
+	}
+	
+	public Mensagem(TipoMensagem tipoMensagem, String conteudoMensagem){
+		this(tipoMensagem);
+		adicionarConteudo(conteudoMensagem);
+	
+	}
+	
+	public void adicionarConteudo(String texto) {
+		adicionarConteudo(new Label(texto));
 	}
 
 	public TipoMensagem getTipoMensagem() {
@@ -62,8 +80,53 @@ public class Mensagem extends VBox {
 		if(conteudo instanceof Label)((Label)conteudo).setWrapText(true);
 		ChatBotController.getMainScene().getWindow().widthProperty().addListener((obs, oldVal, newVal) -> {
 			pane.setMaxWidth((double) newVal/2);
+			ChatBotController.setChatPaneWidth((double) newVal);
+			
+			if(accordion != null) {
+				accordion.setPrefWidth((double)newVal / 2);
+				percorrerAccordion( (double) newVal/2);
+			}
 		});
 
+	}
+	
+	private void percorrerAccordion(double d) {
+		for(Node node : accordion.getChildrenUnmodifiable()) {
+			for(Node p : ((TitledPane)node).getChildrenUnmodifiable())
+			if(node instanceof Label)((Label)p).setText("SHASHKJDHSKJDHK");
+		}
+	}
+
+	public void adicionarConteudoExpandivel(String titulo, Node... conteudos) {
+		VBox vbox = new VBox();
+		TitledPane titledPane = new TitledPane(titulo, vbox);
+		
+		accordion = new Accordion(titledPane);
+		accordion.setExpandedPane(titledPane);
+		
+		for(Node node: conteudos) {
+			
+			formatarLabel(node);
+			vbox.getChildren().add(node);
+		}
+		adicionarConteudo(accordion);
+	}
+
+	/**Formata um label. Se o node não for um label, não faz nada e retorna falso**/
+	private boolean formatarLabel(Node node) {
+		if(!(node instanceof Label)) return false;
+		Label label = (Label) node;
+		label.setWrapText(true);
+		label.setPrefWidth(ChatBotController.getChatPaneWidth()/2);
+		
+		resizeElement(label);
+		return true;
+	}
+	
+	private void resizeElement(Label n) {
+		ChatBotController.getMainScene().getWindow().widthProperty().addListener((obs, oldVal, newVal) -> {
+			n.setMaxWidth(ChatBotController.getChatPaneWidth()/2);
+		});
 	}
 
 }
